66d2ad32e3509a778cb31ac5eedd4f172e20279a
killyoudarlings <jlee286@artic.edu>
2020-01-28 15:47:41 -0600

    dd
    d

---
+++ docs/code/in_class/day01/01_RozinMirror/scripts/main.js
@@ -0,0 +1,37 @@
+let myCapture;
+
+function setup() {
+  createCanvas(320, 240);
+  myCapture = createCapture(VIDEO);
+  //myCapture.hide();
+  fill(255, 0, 0);
+  noStroke();
+}
+
+function draw() {
+  background(255);
+  //fill(random(255), random(255), random(255));
+  
+  //load pixel data into myCapture object
+  myCapture.loadPixels();
+ 
+  
+  const stepSize = round(constrain(mouseX / 8, 6, 32));
+  
+  for(let y = 0; y < height; y+=stepSize) {
+    for(let x = 0; x < width; x+=stepSize) {
+      const i = y * width + x;
+      const darkness = (255 - myCapture.pixels[i * 4]) / 255;
+      const radius = stepSize * darkness;
+      
+      push();
+        scale(-1, 1);
+        translate(width, 0);
+        rect(x, y, radius, radius);
+      pop();
+      
+      fill(random(255), random(255), random(255));
+      rect(x, y, radius, random(1,30));
+    }
+  }
+}
